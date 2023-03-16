805ba43a80cb3f473019261af37b7bb0c13b440d
Shela Ng <shela_ng@shelas-air.mynet>
2023-03-16 13:08:43 +0800

    checkbox trial 3

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.route("/exe/irregular-verb")
 .get(async(req,res) => { try {
     const foundVerbs = await Verb.find();
     const onOffSwitch =req.body.onOffSwitch;
-    res.render("irregularVerb", {verbArray:foundVerbs, onOffSwitch:onOffSwitch});
+    res.render("irregularVerb", {verbArray:foundVerbs});
     } catch(err){
     res.send(err)
 }
